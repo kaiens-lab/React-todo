885bcaddd89a6b44842c8ed270cf2bd1b7f5c904
KN <ckn.kaien@gmail.com>
2024-11-06 20:38:06 +0800

    Remove unuse hook.

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Input = ({ todos, setTodos, setFilteredTodos }) => {
   let [input, setInput] = useState("");
